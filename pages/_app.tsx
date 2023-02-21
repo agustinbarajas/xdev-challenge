59922e85d5f25e46c9be7355451d43b2f4d0058e
Agustin Barajas <agustinbvaldivia@gmail.com>
2023-02-20 19:07:00 -0700

    Add client emotion cache configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
+import createEmotionCache from "@/utils/createEmotionCache";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache: EmotionCache;
+}
+
+export default function App({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <CssBaseline enableColorScheme />
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
